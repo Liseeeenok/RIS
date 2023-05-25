b298da96c0f9f5c7c4785520ece3bbe63904eb70
Evgeniy Fereferov <vlad.vedernikov.95@mail.ru>
2023-05-26 02:00:46 +0800

    merge branch

---
+++ storage/cms/cache/3e/2a/404.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms646fa21a556c3453188028_c6fee99031e7d875ec244e323c9e8915Class extends Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/cache/ff/f6/wsr.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms646fa02d0d807576029491_8d034f519d3fd3513aa8eb12497d6761Class extends Cms\Classes\PageCode
+{
+
+}
